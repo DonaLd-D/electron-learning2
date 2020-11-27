9c9ccd2dcf6becbfeb0c974b8642b8b3fdf854e9
DonaLd-D <1376825153@qq.com>
2020-11-27 11:21:47 +0800

    DIY window

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ const datas = {
 };
 app.on('ready', () => {
   const win = new BrowserWindow({
+    width: 960,
+    height: 540,
+    frame: false, // 隐藏默认边框和按钮
+    resizable: false, // 不可拖拽改变窗口大小
     webPreferences: {
         nodeIntegration: true,
         enableRemoteModule: true
     }
   });
+
   win.webContents.openDevTools(); //打开调试面板
   win.loadFile('./layout/index.html'); //载入web页面
 
